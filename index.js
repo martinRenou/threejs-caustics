6c64ddb3a29c16a9d93d15c37469043dc996fd92
martinRenou <martin.renou@gmail.com>
2020-02-11 16:35:31 +0100

    Add boundaries meshes
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ camera.position.y = -3;
 
 const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true, alpha: true});
 renderer.setSize(width, height);
-renderer.setClearColor(new THREE.Color('white'));
 
 
 // Create mouse Controls
@@ -28,6 +27,33 @@ controls.rotateSpeed = 2.5;
 controls.zoomSpeed = 1.2;
 controls.panSpeed = 0.9;
 controls.dynamicDampingFactor = 0.9;
+controls.target.set(1.61, 0.25, 0.25);
+
+
+// Create lights
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+
+
+// Create bounds meshes
+const box = new THREE.Mesh(
+  new THREE.BoxGeometry(0.16, 0.4, 0.16),
+  new THREE.MeshBasicMaterial()
+);
+box.position.x = 2.47
+box.position.y = 0.5
+box.position.z = 0.08
+scene.add(box);
+
+const boundaries = new THREE.Mesh(
+    new THREE.BoxGeometry(3.22, 1., 1.),
+    new THREE.MeshBasicMaterial()
+);
+boundaries.material.side = THREE.BackSide;
+boundaries.position.x = 1.61
+boundaries.position.y = 0.5
+boundaries.position.z = 0.5
+scene.add(boundaries);
 
 
 // Function that fetches the buffers (vertices/triangle indices)
@@ -58,10 +84,11 @@ Promise.all([fetchArray('/data/vertices32_25.bin', Float32Array), fetchArray('/d
   const geometry = new THREE.BufferGeometry();
   geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
   geometry.setIndex(new THREE.BufferAttribute(triangles, 1));
-  geometry.center();
 
-  const material = new THREE.MeshBasicMaterial({color: 'blue'});
+  const material = new THREE.MeshBasicMaterial({color: '#87b3d4'});
   material.side = THREE.DoubleSide;
+  material.transparent = true;
+  material.opacity = 0.7;
 
   const mesh = new THREE.Mesh(geometry, material);
 
@@ -69,6 +96,7 @@ Promise.all([fetchArray('/data/vertices32_25.bin', Float32Array), fetchArray('/d
 });
 
 
+// Main rendering loop
 function animate() {
   renderer.render(scene, camera);
 
