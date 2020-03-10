77ca63e133df2eb72a2f2a30e8580439276ac5b4
martinRenou <martin.renou@gmail.com>
2020-03-10 12:11:33 +0100

    Fix projection
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- shaders/water/water_vertex.glsl
+++ shaders/water/water_vertex.glsl
@@ -11,9 +11,9 @@ void main() {
   vec3 axis_x = vec3(modelViewMatrix[0].x, modelViewMatrix[1].x, modelViewMatrix[2].x);
   vec3 axis_y = vec3(modelViewMatrix[0].y, modelViewMatrix[1].y, modelViewMatrix[2].y);
   vec3 axis_z = vec3(modelViewMatrix[0].z, modelViewMatrix[1].z, modelViewMatrix[2].z);
-  vec3 offset = vec3(- modelViewMatrix[0].w, - modelViewMatrix[1].w, - modelViewMatrix[2].w);
+  vec3 offset = vec3(modelViewMatrix[0].w, modelViewMatrix[1].w, modelViewMatrix[2].w);
 
-  eye = vec3(dot(offset, axis_x), dot(offset, axis_y), dot(offset, axis_z));
+  eye = vec3(dot(-offset, axis_x), dot(-offset, axis_y), dot(-offset, axis_z));
 
-  gl_Position = modelViewMatrix * projectionMatrix * vec4(pos, 1.0);
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
