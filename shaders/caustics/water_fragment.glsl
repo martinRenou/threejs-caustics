81849bb48504b2588b727205431893000f674161
martinRenou <martin.renou@gmail.com>
2020-06-24 17:41:54 +0200

    Debug NVidia drivers
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- shaders/caustics/water_fragment.glsl
+++ shaders/caustics/water_fragment.glsl
@@ -14,7 +14,17 @@ void main() {
     float oldArea = length(dFdx(oldPosition)) * length(dFdy(oldPosition));
     float newArea = length(dFdx(newPosition)) * length(dFdy(newPosition));
 
-    causticsIntensity = causticsFactor * oldArea / newArea;
+    float ratio;
+
+    // Prevent dividing by zero (debug NVidia drivers)
+    if (newArea == 0.) {
+      // Arbitrary large value
+      ratio = 2.0e+20;
+    } else {
+      ratio = oldArea / newArea;
+    }
+
+    causticsIntensity = causticsFactor * ratio;
   }
 
   gl_FragColor = vec4(causticsIntensity, 0., 0., depth);
