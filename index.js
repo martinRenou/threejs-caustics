6edea3df65df1e53f02ee3d07452ae4833616339
martinRenou <martin.renou@gmail.com>
2020-06-24 13:10:53 +0200

    Translate rock
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- index.js
+++ index.js
@@ -139,14 +139,14 @@ const rockLoaded = new Promise((resolve) => {
   objLoader.load('rock.obj', (rockGeometry) => {
     rockGeometry = rockGeometry.children[0].geometry;
     rockGeometry.computeVertexNormals();
-    rockGeometry.scale(0.05, 0.05, 0.05);
-    // rockGeometry.rotateZ(-Math.PI / 2.);
-    rockGeometry.translate(0.5, 0, 0.1);
 
-    rock1 = rockGeometry;
+    rock1 = new THREE.BufferGeometry().copy(rockGeometry);
+    rock1.scale(0.05, 0.05, 0.02);
+    rock1.translate(0.2, 0., 0.1);
 
     rock2 = new THREE.BufferGeometry().copy(rockGeometry);
-    rock2.translate(-1., 0.5, 0.1)
+    rock2.scale(0.05, 0.05, 0.05);
+    rock2.translate(-0.5, 0.5, 0.2);
     rock2.rotateZ(Math.PI / 2.);
 
     resolve();
