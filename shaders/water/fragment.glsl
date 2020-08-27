8bb93d6524b2da1e068ab34f815653256651bd74
martinRenou <martin.renou@gmail.com>
2020-08-27 10:53:45 +0200

    Add chromatic aberration for the water refractive effect
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- shaders/water/fragment.glsl
+++ shaders/water/fragment.glsl
@@ -1,14 +1,20 @@
 uniform sampler2D envMap;
 uniform samplerCube skybox;
 
-varying vec2 refractedPosition;
+varying vec2 refractedPosition[3];
 varying vec3 reflected;
 varying float reflectionFactor;
 
 
 void main() {
-  vec3 refractedColor = texture2D(envMap, refractedPosition * 0.5 + 0.5).xyz;
+  // Color coming from the sky reflection
   vec3 reflectedColor = textureCube(skybox, reflected).xyz;
+
+  // Color coming from the environment refraction, applying chromatic aberration
+  vec3 refractedColor = vec3(1.);
+  refractedColor.r = texture2D(envMap, refractedPosition[0] * 0.5 + 0.5).r;
+  refractedColor.g = texture2D(envMap, refractedPosition[1] * 0.5 + 0.5).g;
+  refractedColor.b = texture2D(envMap, refractedPosition[2] * 0.5 + 0.5).b;
 
   gl_FragColor = vec4(mix(refractedColor, reflectedColor, clamp(reflectionFactor, 0., 1.)), 1.);
 }
